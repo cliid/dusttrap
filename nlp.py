a1cf0b607227d304a51a8fd2cb60be692b101c1d
HackerJang <jiwujang@naver.com>
2019-12-09 14:28:40 +0900

    v1.0a.1000.01.r1 beta release!

--- nlp.py
+++ nlp.py
@@ -9,4 +9,4 @@ def return_intent(project_id, session_id, text, language_code):
         text_input = df.types.TextInput(text=text, language_code=language_code)
         query_input = df.types.QueryInput(text=text_input)
         response = session_client.detect_intent(session=session, query_input=query_input)
-        return response.query_result.intent
+        return response.query_result.intent.display_name
